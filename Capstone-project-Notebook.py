fd316cb40a278b80f109c31da5df96f8606dc4b4
ROSA MARIA CALDERON OLAGUIVEL <rmcalder@pe.ibm.com>
2019-02-05 21:04:28 -0500

    add watson studio script

---
+++ Capstone-project-Notebook.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+#Notebook to test the use of the repository in Github
+
+
+# In[ ]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+import numpy as np
+
+
+# In[1]:
+
+
+print ("Hello Capstone Project Course!")
+
